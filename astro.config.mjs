66aa5be496dd2cebffda1e19ce201fd71d42de7a
Tahir Butt <tb@lotus.yt>
2026-03-13 23:06:37 -0400

    [cbfs-wbu.7] feat: add sitemap, configure trailing slash handling and site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://blackfreedomstudies.org',
+  trailingSlash: 'ignore',
+  integrations: [sitemap()],
+});
